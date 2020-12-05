b4d477ea1c79488cb5fdb66f27572d9c67e6f786
richie <richie.morrisroe@gmail.com>
2020-12-05 13:09:42 +0000

    update app a little

--- property_app/app.R
+++ property_app/app.R
@@ -10,27 +10,47 @@ elec <- readRDS("../electoral_district_aggregates.rds")
 get_unique_values <- function(df, var) {
     r <- unique(eval(substitute(var), df))
 }
-year_uniq  <- get_unique_values(elec, year)
+year_uniq  <- (get_unique_values(elec, year))
 county_choices <- get_unique_values(elec, COUNTYNAME)
 elec_name_uniq  <- get_unique_values(elec, EDNAME)
+address_uniq <- get_unique_values(ppr_sf, input_string)
 ui <- pageWithSidebar(
     headerPanel("Property Dashboard"),
     sidebarPanel(selectInput("year", "Year:", choices = year_uniq, selected="2018"),
-                 checkboxGroupInput("county", "Year:", choices = county_choices,
+                 radioButtons("switch", "Map or Address View", choices=list("map", "address")),
+
+                 checkboxGroupInput("county", "Location:",
+                                    choices = county_choices,
                                     selected=county_choices),
-                 sliderInput("price", "Price", min=0, max=2e6, value=c(0, 300000))
+                 sliderInput("price", "Price",
+                             min=0, max=1e6, value=c(0, 300000)),
+                 selectizeInput("add", "Address:",
+                                choices=address_uniq,
+                                selected=address_uniq[1],
+                                multiple=TRUE)
                  ),
     mainPanel(
-        plotOutput("map", width="800px", height="800px")
+            plotOutput("map", width="600px", height="600px"),
+            tableOutput("add_df")
+        
     )
 
 )
 
-server <- function(input, output) {
-    
-    output$map <- renderPlot({
-        ggmap::ggmap(dubmap)+geom_point(data=filter(ppr_sf, year==input$year, price>input$price[1],  price<input$price[2]), aes(x=longitude, y=latitude, colour=(price)))
+server <- function(input, output, session) {
+
+    output$map <-
+        renderPlot({
+            if(input$switch=="map") {
+        ggmap::ggmap(dubmap)+geom_point(data=filter(ppr_sf, year==input$year, price>input$price[1],  price<input$price[2], geo_county %in% input$county), aes(x=longitude, y=latitude, colour=(price))) }
     })
+
+
+
+    output$add_df <- renderTable(
+            if (input$switch=="address"){
+                filter(ppr_sf, grepl(input$add, x=input_string))
+            })
 
 }
 
